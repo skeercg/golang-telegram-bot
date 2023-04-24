a4e47c75bf31ca4e2197df0295e67dff904778bd
Alikhan <a.abikhas@bytepride.dev>
2023-04-24 15:00:07 +0600

    done

---
+++ cmd/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/spf13/viper"
+	"golang-telegram-bot/pkg"
+	"log"
+)
+
+func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing configs: %s", err.Error())
+	}
+
+	unsplashKey := viper.GetString("unsplashkey")
+	imageController := pkg.ImageController{
+		UnsplashKey: unsplashKey,
+	}
+	bot, err := tgbotapi.NewBotAPI(viper.GetString("tgtoken"))
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	updateConfig := tgbotapi.NewUpdate(0)
+	updateConfig.Timeout = 60
+
+	updates := bot.GetUpdatesChan(updateConfig)
+
+	for update := range updates {
+		go func() {
+			if update.Message != nil {
+				chatID := update.Message.Chat.ID
+				msg := tgbotapi.NewMessage(chatID, "")
+
+				switch update.Message.Text {
+				case "/start":
+					msg.Text = "Hello, I'm Imager bot!"
+
+					if _, err := bot.Send(msg); err != nil {
+						log.Panic(err)
+					}
+				case "/image":
+					photoConfig := imageController.GetPhoto(chatID)
+
+					if _, err := bot.Send(photoConfig); err != nil {
+						log.Panic(err)
+					}
+				default:
+					msg.Text = "Unknown command"
+
+					if _, err := bot.Send(msg); err != nil {
+						log.Panic(err)
+					}
+				}
+
+			}
+		}()
+	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
+}
---
+++ pkg/image_controller.go
@@ -0,0 +1,61 @@
+package pkg
+
+import (
+	"encoding/json"
+	"fmt"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
+
+type UnsplashResponse struct {
+	URLs `json:"urls"`
+}
+
+type URLs struct {
+	Regular string `json:"regular"`
+}
+
+type ImageController struct {
+	UnsplashKey string
+}
+
+func (c *ImageController) GetPhoto(ChatID int64) tgbotapi.PhotoConfig {
+	response, err := http.Get(fmt.Sprintf("https://api.unsplash.com/photos/random?client_id=%s", c.UnsplashKey))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer response.Body.Close()
+
+	var unsplashResponse UnsplashResponse
+	if err := json.NewDecoder(response.Body).Decode(&unsplashResponse); err != nil {
+		log.Fatal(err)
+	}
+
+	imageURL := unsplashResponse.URLs.Regular
+
+	response, err = http.Get(imageURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer response.Body.Close()
+
+	fileName := "image.jpg"
+
+	file, err := os.Create(fileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	imageData, err := io.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	photoConfig := tgbotapi.NewPhoto(ChatID, tgbotapi.FileBytes{Name: fileName, Bytes: imageData})
+
+	return photoConfig
+}
